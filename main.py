5b9e77a65a6afb8d4b92fd890761ee033e81673c
Michael Simon <michael.simon@gmail.com>
2022-02-20 18:43:14 -0500

    handle http errors

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import pandas as pd
-from flask import Flask
-from flask import render_template
-from flask import request
-
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -29,3 +25,8 @@ def search():
         search_term = request.form["search_term"]
         results = all_results[all_results["Applicant"].str.contains(search_term, case=False) | all_results["FoodItems"].str.contains(search_term, case=False) | all_results["Address"].str.contains(search_term, case=False)]
         return render_template("list.html", title=title, data=results)
+
+@app.errorhandler(500)
+def error():
+    title = "Error"
+    return render_template("error.html", title=title)
