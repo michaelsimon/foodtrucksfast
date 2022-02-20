56c42bcd2a8626fdf59a7afd6dc9a649a33e5085
Michael Simon <michael.simon@gmail.com>
2022-02-20 18:27:37 -0500

    sort data by applicant, allow serarching by food items and address as well

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from crypt import methods
 import pandas as pd
 from flask import Flask
 from flask import render_template
@@ -9,18 +8,18 @@ from flask import request
 app = Flask(__name__)
 
 df = pd.read_csv('https://data.sfgov.org/api/views/rqzj-sfat/rows.csv')
-df2 = df[["Applicant","Address","FoodItems", "Latitude","Longitude","Status"]]
+df2 = df[["Applicant","Address","FoodItems", "Latitude","Longitude","Status"]].sort_values(by=['Applicant'])
 all_results = df2[df2["Status"].str.contains("APPROVED")]
 
 @app.route("/")
 def index():
-    title = "Home"
+    title = "Welcome"
     return render_template("index.html", title=title)
 
 
 @app.route("/list")
 def get_list():
-    title = "Food Truck List"
+    title = "All Your Options"
     return render_template("list.html", title=title, data=all_results)
 
 @app.route("/search", methods=['POST'])
@@ -28,5 +27,5 @@ def search():
     title = "Search"
     if request.method == "POST":
         search_term = request.form["search_term"]
-        results = all_results[all_results["Applicant"].str.contains(search_term, case=False)]
+        results = all_results[all_results["Applicant"].str.contains(search_term, case=False) | all_results["FoodItems"].str.contains(search_term, case=False) | all_results["Address"].str.contains(search_term, case=False)]
         return render_template("list.html", title=title, data=results)
