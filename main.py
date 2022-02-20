c854fa2e01a9809b3777f09da0e886295bbcdcfe
Michael Simon <michael.simon@gmail.com>
2022-02-20 16:24:16 -0500

    begin to templatize application, render data from dataframe to output

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pandas as pd
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Welcome"
+    title = "Home"
+    return render_template("index.html", title=title)
+
 
 @app.route("/list")
 def get_list():
+    title = "Food Truck List"
     df = pd.read_csv('https://data.sfgov.org/api/views/rqzj-sfat/rows.csv')
     df2 = df[["Applicant","Address","FoodItems", "Latitude","Longitude","Status"]]
-    return df2[df2["Status"].str.contains("APPROVED")].to_html()
+    # df2[df2["Status"].str.contains("APPROVED")].to_html()
+    return render_template("list.html", title=title, data=df2[df2["Status"].str.contains("APPROVED")])
